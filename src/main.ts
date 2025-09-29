fef02efd2a483d7ddb9b5d0d8291c50c055d6ff2
Mayank Ahuja <mayankahuja8894@gmail.com>
2025-09-29 11:01:11 +0530

    updated project structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { Header } from './app/shared/components/header/header';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
-bootstrapApplication(Header,appConfig).catch((err) => console.error(err));
