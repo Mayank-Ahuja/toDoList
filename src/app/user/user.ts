e424b446304a2baf3f0c294aca865e6a24d34c5e
Mayank Ahuja <mayankahuja8894@gmail.com>
2025-11-22 02:26:36 +0530

    added dynamic iamges of users from local data

---
+++ src/app/user/user.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.scss'
+})
+
+export class User {
+
+  selectedUser = DUMMY_USERS[randomIndex];
+
+}
