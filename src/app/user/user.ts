9972cb71c93f027fd2cf66067e64a15196bbaeb3
Mayank Ahuja <mayankahuja8894@gmail.com>
2025-11-25 02:10:04 +0530

    updated image and event handling

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -14,4 +14,15 @@ export class User {
 
   selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath():string {
+    return 'images/users/'+ this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log('clicked on: ', this.selectedUser);
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+    console.log('user after update: ', this.selectedUser);
+  }
+
 }
